58362822495bf254bd828e2fbcf03c6b745a3681
rodonggyun <102853290+rodonggyun@users.noreply.github.com>
2025-06-30 20:08:19 +0900

    Initial commit

---
+++ Assets/C#/StartClick.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartClick : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("GameScene1");
+        }
+    }
+}
